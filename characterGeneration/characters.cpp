82ca594338d994d2437001558784c4ebd8c47de7
Rinnejade <vinodkumars767.vs@gmail.com>
2017-04-10 17:27:11 +0530

    7 segment character generation complete

--- characterGeneration/characters.cpp
+++ characterGeneration/characters.cpp
@@ -2,40 +2,59 @@
 #include <string>
 #include <math.h>
 #include <GL/glut.h>
+#include <unistd.h>
 #define PI 3.14159
 using namespace std;
-float radius = 10, con = 0;
-float yes[3] = {0, 1, 1};
+int con = 100, base = 200, width = 10, height = 100, diff = 5;
+float radius = 10;
+float yes[3] = {1, 1, 0};
 float no[3] = {0, 0, 0};
-int a[13][9];
-void readBitmap(){
-  for (size_t i = 0; i < 13; i++)
-    for (size_t j = 0; j < 9; j++)
-        cin>>a[i][j];
-}
-void drawFilledCircle(float radius, float x, float y, float color[3]){
-	int triangleAmount = 20; //# of triangles used to draw circle
-	GLfloat twicePi = 2.0f * PI;
+int c[16][7]={
+  {1, 1, 1, 1, 1, 1, 0},
+  {0, 0, 0, 1, 1, 0, 0},
+  {1, 0, 1, 1, 0, 1, 1},
+  {0, 0, 1, 1, 1, 1, 1},
+  {0, 1, 0, 1, 1, 0, 1},
+  {0, 1, 1, 0, 1, 1, 1},
+  {1, 1, 1, 0, 1, 1, 1},
+  {0, 0, 1, 1, 1, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1},
+  {0, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 1, 0, 1},
+  {1, 1, 0, 0, 1, 1, 1},
+  {1, 1, 1, 0, 0, 1, 0},
+  {1, 0, 0, 1, 1, 1, 1},
+  {1, 1, 1, 0, 0, 1, 1},
+  {1, 1, 1, 0, 0, 0, 1}
+};
+int segment[7][8]={
+  {base, base, base+width, base, base+width, base+height, base, base+height},
+  {base, base+2*width+height, base+width, base+2*width+height, base+width, base+2*width+2*height, base, base+2*width+2*height},
+  {base, base+2*width+2*height+diff, base+con+width, base+2*width+2*height+diff, base+con+width, base+2*width+2*height+diff+width, base, base+2*width+2*height+diff+width },
+  {base+con, base+2*width+height, base+width+con, base+2*width+height, base+width+con, base+2*width+2*height, base+con, base+2*width+2*height},
+  {base+con, base, base+width+con, base, base+width+con, base+height, base+con, base+height},
+  {base, base-width+diff, base+con+width, base-width+diff, base+con+width, base-2*width+diff, base, base-2*width+diff },
+  {base, base+width+height-diff, base+con+width, base+width+height-diff, base+con+width, base+2*width+height-diff, base, base+2*width+height-diff },
+  };
+void drawRect(float color[3], int i){
   glColor3fv(color);
-	glBegin(GL_TRIANGLE_FAN);
-		glVertex2f(x, y); // center of circle
-		for(int i = 0; i <= triangleAmount;i++)
-      glVertex2f(x + (radius * cos(i *  twicePi / triangleAmount)), y + (radius * sin(i * twicePi / triangleAmount)));
-	glEnd();
+    glBegin(GL_POLYGON);
+        glVertex2i(segment[i][0],segment[i][1]);
+        glVertex2i(segment[i][2],segment[i][3]);
+        glVertex2i(segment[i][4],segment[i][5]);
+        glVertex2i(segment[i][6],segment[i][7]);
+    glEnd();
   glFlush();
 }
-void generateCharacters(){
-    int x =150, y= 400;
-  for (size_t i = 0; i < 13; i++) {
-    for (size_t j = 0; j < 9; j++) {
-      if(a[i][j])
-        drawFilledCircle(radius, x, y, yes);
+void draw7segment(void){
+  for (size_t i = 0; i < 16; i++) {
+    for (size_t j = 0; j < 7; j++) {
+      if(c[i][j])
+        drawRect(yes, j);
       else
-        drawFilledCircle(radius, x, y, no);
-      x+=2*(radius-con);
+        drawRect(no, j);
     }
-    y-=2*(radius-con);
-    x = 150;
+    usleep(2000 * 1000); // takes microseconds
   }
 }
 void Init()
@@ -52,8 +71,7 @@ int main(int argc, char **argv)
 {
   glutInit(&argc,argv);
   Init();
-  readBitmap();
-  glutDisplayFunc(generateCharacters);
+  glutDisplayFunc(draw7segment);
   glutMainLoop();
   return 0;
 }
