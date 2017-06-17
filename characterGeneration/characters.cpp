d595512edf63f224d7fff0be6460535771f2327a
Rinnejade <vinodkumars767.vs@gmail.com>
2017-06-17 10:31:30 +0530

    updated character generation

--- characterGeneration/characters.cpp
+++ characterGeneration/characters.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <algorithm>
 #include <GL/glut.h>
 #include <unistd.h>
+#include <vector>
 #define PI 3.14159
 using namespace std;
+typedef  pair<int, int> iPair;
 int con = 100, base = 200, width = 10, height = 100, diff = 5;
 float radius = 10;
 float yes[3] = {1, 1, 0};
@@ -27,32 +30,31 @@ int c[16][7]={
   {1, 1, 1, 0, 0, 1, 1},
   {1, 1, 1, 0, 0, 0, 1}
 };
-int segment[7][8]={
-  {base, base, base+width, base, base+width, base+height, base, base+height},
-  {base, base+2*width+height, base+width, base+2*width+height, base+width, base+2*width+2*height, base, base+2*width+2*height},
-  {base, base+2*width+2*height+diff, base+con+width, base+2*width+2*height+diff, base+con+width, base+2*width+2*height+diff+width, base, base+2*width+2*height+diff+width },
-  {base+con, base+2*width+height, base+width+con, base+2*width+height, base+width+con, base+2*width+2*height, base+con, base+2*width+2*height},
-  {base+con, base, base+width+con, base, base+width+con, base+height, base+con, base+height},
-  {base, base-width+diff, base+con+width, base-width+diff, base+con+width, base-2*width+diff, base, base-2*width+diff },
-  {base, base+width+height-diff, base+con+width, base+width+height-diff, base+con+width, base+2*width+height-diff, base, base+2*width+height-diff },
-  };
-void drawRect(float color[3], int i){
+vector<iPair> endPoints = { {200, 200}, {200, 300}, {205, 400}, {280, 300}, {280, 200}, {205, 200}, {205, 295} };
+bool horizontalOrientation[7] = {false, false, true, false, false, true, true};
+void drawLine(float x1, float y1, float color[3], bool horizontal){
+  float x2 = x1 , y2 = y1;
+  if(horizontal)
+    x2 = x1+70;
+  else
+    y2 = y1+90;
   glColor3fv(color);
-    glBegin(GL_POLYGON);
-        glVertex2i(segment[i][0],segment[i][1]);
-        glVertex2i(segment[i][2],segment[i][3]);
-        glVertex2i(segment[i][4],segment[i][5]);
-        glVertex2i(segment[i][6],segment[i][7]);
-    glEnd();
+  // glPointSize(100);
+  glBegin(GL_LINES);
+    glVertex2i(x1, y1);
+    glVertex2i(x2, y2);
+  glEnd();
   glFlush();
 }
 void draw7segment(void){
   for (size_t i = 0; i < 16; i++) {
     for (size_t j = 0; j < 7; j++) {
       if(c[i][j])
-        drawRect(yes, j);
+      drawLine(endPoints[j].first, endPoints[j].second, yes, horizontalOrientation[j]);
+        // drawRect(yes, j);
       else
-        drawRect(no, j);
+        drawLine(endPoints[j].first, endPoints[j].second, no, horizontalOrientation[j]);
+        // drawRect(no, j);
     }
     usleep(2000 * 1000); // takes microseconds
   }
