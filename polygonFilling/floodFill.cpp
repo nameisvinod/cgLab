0a4d4ba968e58864d445e5af3d53db73df0553cc
Rinnejade <vinodkumars767.vs@gmail.com>
2017-06-17 14:01:12 +0530

    corrected

--- polygonFilling/floodFill.cpp
+++ polygonFilling/floodFill.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <list>
 #include <queue>
 #include <set>
 #include <iostream>
@@ -18,6 +19,7 @@ public:
 		y=y1;
 	}
 };
+std::list<Point> subjectPolygon ;
 int ww = 600, wh = 500;
 float bgCol[3] = {0.0, 0.0,0.0};
 float intCol[3] = {1.0,0.0,0.0};
@@ -34,15 +36,24 @@ void setPixel(int pointx, int pointy, float f[3])
 void getPixel(int x, int y, float pixels[3])
 {
      glReadPixels(x,y,1.0,1.0,GL_RGB,GL_FLOAT,pixels);
+		 cout<<"pixels : "<<pixels[0]<<"	:	"<<pixels[1]<<"	:	"<<pixels[2]<<"	:	"<<endl;
+
 }
-void drawPolygon(int x1, int y1, int x2, int y2)
+void initPolygon(){
+	int n, x, y;
+	// cout<<"Enter number of vertices : ";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>x>>y;
+		subjectPolygon.push_back(Point(x, y));
+	}
+}
+void drawPolygon()
 {
        glColor3f(1.0, 0.0, 0.0);
        glBegin(GL_POLYGON);
-            glVertex2i(x1, y1);
-            glVertex2i(x1, y2);
-            glVertex2i(x2, y2);
-            glVertex2i(x2, y1);
+			 for (Point point : subjectPolygon)
+			 	glVertex2i(point.x,point.y);
        glEnd();
        glFlush();
 }
@@ -50,12 +61,14 @@ void display()
 {
        glClearColor(0.0,0.0,0.0,1.0);
        glClear(GL_COLOR_BUFFER_BIT);
-       drawPolygon(100,100,200,200);
+       drawPolygon();
        glFlush();
 }
 int checkPoint(Point p, float oldcolor[3], float newcolor[3]){
 	float color[3];
 	getPixel(p.x,p.y,color);
+	// cout<<"curcolor : "<<color[0]<<"	:	"<<color[1]<<"	:	"<<color[2]<<"	:	"<<endl;
+	// cout<<"oldcolor : "<<oldcolor[0]<<"	:	"<<oldcolor[1]<<"	:	"<<oldcolor[2]<<"	:	"<<endl;
   int isOldColor = color[0]==oldcolor[0] && (color[1])==oldcolor[1] && (color[2])==oldcolor[2] ;
 	int notNewColor = (color[0]!=newcolor[0]) || (color[1]!=newcolor[1]) || (color[2]!=newcolor[2]) ;
 	return isOldColor;
@@ -95,9 +108,9 @@ void floodFill(Point seed,float oldcolor[3],float newcolor[3])
 					setPixel(i, seed.y, newcolor);
 					Point north(i, seed.y+1);
 					Point south(i, seed.y-1);
-					if(checkPoint(north,oldcolor,newcolor))
+					// if(checkPoint(north,oldcolor,newcolor))
 						Q.push(north);
-					if(checkPoint(south,oldcolor,newcolor))
+					// if(checkPoint(south,oldcolor,newcolor))
 						Q.push(south);
 				}
 			}
@@ -121,6 +134,7 @@ int main(int argc, char** argv)
        glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
        glutInitWindowSize(ww,wh);
        glutCreateWindow("Flood-Fill-Recursive");
+			 initPolygon();
        glutDisplayFunc(display);
        myinit();
        glutMouseFunc(mouse);
